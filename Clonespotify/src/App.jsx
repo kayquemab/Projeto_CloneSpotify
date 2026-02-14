78dc8c301549924bd593c11614fca30d6522aefa
Kayque Miqueias <kayquemiqueias17@gmail.com>
2026-02-14 18:18:02 -0300

    ajuste

--- Clonespotify/src/App.jsx
+++ Clonespotify/src/App.jsx
@@ -1,11 +1,12 @@
 import Player from "./components/Player";
 import Sidebar from "./components/sidebar";
 
+
 const App = () => {
   return (
     <div className="h-screen bg-black">
       <div className="h-[90%] flex">
-        <Sidebar />
+        <Sidebar/>
       </div>
       <Player />
     </div>
