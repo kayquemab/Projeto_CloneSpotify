f45d23504230250332037c4164d8a7ad2fb8ee75
Kayque Miqueias <kayquemiqueias17@gmail.com>
2026-02-18 09:15:31 -0300

    Ajustes

--- Clonespotify/src/components/AlbumItem.jsx
+++ Clonespotify/src/components/AlbumItem.jsx
@@ -2,7 +2,7 @@
 
 const AlbumItem = ({ name, image, desc, id }) => {
     return (
-        <div className='min-w-40 p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
+        <div className='min-w-45 p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
             <img className='rounded' src={image} alt="" />
             <p className='font-bold mt-2 mb-1'>{name}</p>
             <p className='text-slate-200 text-sm'>{desc}</p>
